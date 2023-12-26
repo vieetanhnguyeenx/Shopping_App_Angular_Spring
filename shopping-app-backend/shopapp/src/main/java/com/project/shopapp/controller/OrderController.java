24305b7f440d646ffe0936e590f1e71728366be5
NVA <vieetanhnguyeenx@gmail.com>
2023-12-26 15:42:04 +0700

    Update OrderController.java

--- shopping-app-backend/shopapp/src/main/java/com/project/shopapp/controller/OrderController.java
+++ shopping-app-backend/shopapp/src/main/java/com/project/shopapp/controller/OrderController.java
@@ -7,10 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,5 +31,36 @@ public class OrderController {
             throw ApiRequestException.badRequest(errorMsg);
         }
         return ResponseEntity.ok("Create order " + orderDTO);
+    }
+
+    @GetMapping("/{user_id}")
+    public ResponseEntity<?> getOrdersById(@Valid @PathVariable("user_id") Long id) {
+        return ResponseEntity.ok("Orders from user id " + id);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateOrder(
+            @Valid @PathVariable("id") Long id,
+            @RequestBody @Valid OrderDTO orderDTO,
+            BindingResult result
+    ) {
+        if (result.hasErrors()) {
+            List<String> errorMsg = result.getFieldErrors()
+                    .stream()
+                    .map(FieldError::getDefaultMessage)
+                    .toList();
+
+            // TODO: throw exception to global
+            throw ApiRequestException.badRequest(errorMsg);
+        }
+
+        return ResponseEntity.ok("Updated order " + orderDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteOrder(@Valid @PathVariable("id") Long id) {
+
+        //TODO: update active to false
+        return ResponseEntity.ok("Deleted order " + id);
     }
 }
